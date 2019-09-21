ac1bc5b46eb7d6d33ae2cbc20e99b8455a20fe26
Jack <29169102+Jack5079@users.noreply.github.com>
2019-09-21 16:31:13 -0400

    add piping support also makes an h repl 😂

--- cli.js
+++ cli.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 const H = require( './main.js' ) // We use Hify
-console.log( H( process.argv.slice( 2 ).join( ' ' ) ) ) // log the h'd arugements
+if ( process.argv.slice( 2 ).length ) {
+  console.log( H( process.argv.slice( 2 ).join( ' ' ) ) ) // log the h'd arugements
+  process.exit( 0 ) // exit
+}
+
+// this code is for when it's being piped / no arguments
+process.stdin.resume();
+process.stdin.setEncoding( 'utf8' );
+process.stdin.on( 'data', function ( data ) {
+  process.stdout.write( H( data ) );
+} );
